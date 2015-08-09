e94e1f1ebff8b2f787f9bdb75d681cc1ca67c9f9
Oleksii Khilkevych <oleksii@Oleksiis-MBP.fritz.box>
2015-08-09 21:38:30 +0200

    initial commit

---
+++ hands-on-git-webapp/src/main/java/com/payon/cache/IfNoneMatch.java
@@ -0,0 +1,13 @@
+package com.payon.cache;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface IfNoneMatch {
+    /**
+     * TODO: SpEL expression to calculate ETag value
+     * @return ETag for request
+     */
+    String value() default "";
+}
---
+++ hands-on-git-webapp/src/main/java/com/payon/util/ReloadableMessageSource.java
@@ -0,0 +1,12 @@
+package com.payon.util;
+
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+import java.util.*;
+
+public class ReloadableMessageSource extends ReloadableResourceBundleMessageSource {
+
+    public Properties getProperties(Locale locale) {
+        return super.getMergedProperties(locale).getProperties();
+    }
+}
